4c5b112e952c38ad32c5014578d05557b476d311
dumbo93 <31391601+dumbo93@users.noreply.github.com>
2018-06-04 15:16:20 +0200

    Update wifi_client.h

--- wifi_client.h
+++ wifi_client.h
@@ -3,12 +3,7 @@
 #include <arpa/inet.h>
 
 int WIFI_init( int socket_desc );
-int WIFI_create_socket( int socket_desc );
 void WIFI_close_socket( int socket_desc );
-void WIFI_get_connection( int socket_desc );
 int WIFI_send_data( int socket_desc );
-int WIFI_receive_data( int socket_desc );
-
-
 
 #endif 
