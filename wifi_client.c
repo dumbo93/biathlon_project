b22146ef848123d530f026694a4a3fc6c47fb00e
dumbo93 <31391601+dumbo93@users.noreply.github.com>
2018-06-04 15:15:53 +0200

    Update wifi_client.c

--- wifi_client.c
+++ wifi_client.c
@@ -1,10 +1,3 @@
-// http://www.linuxhowtos.org/C_C++/socket.htm (13. april 2018)
-// https://www.binarytides.com/socket-programming-c-linux-tutorial/ (16. april 2018)
-
-// create a socket with socket() system call
-// Connect to remote server
-// Send some data
-// Receive a reply
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h> 
@@ -17,24 +10,29 @@
 int WIFI_init( int socket_desc )
 {
 	printf("Hello from wifi_init\n");
-    int sendt_data, received_data;
-    socket_desc = WIFI_create_socket(socket_desc);
-    WIFI_get_connection( socket_desc );
-    //sendt_data = WIFI_send_data( socket_desc, message);
-    //received_data = WIFI_receive_data( socket_desc );
-    //WIFI_close_socket( socket_desc );
-    return socket_desc;
-}
-
-int WIFI_create_socket( int socket_desc )
-{
+    	int sendt_data, received_data;
+	struct sockaddr_in server;
+	
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket\n");
-    }
-    return socket_desc;
+    	if (socket_desc == -1)
+    	{
+       	 	printf("Could not create socket\n");
+    	}
+	
+	
+	server.sin_addr.s_addr = inet_addr("129.241.154.24");
+    	// IP for PC used: 129.241.154.24
+	server.sin_family = AF_INET;
+    	server.sin_port = htons( 8888 );
+
+    	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+   	{
+        	printf("connect error\n");
+    	}
+     
+    	printf("Connected\n");
+
+    	return socket_desc;
 }
 
 
@@ -43,23 +41,6 @@ void WIFI_close_socket( int socket_desc )
 	close(socket_desc);
 }
 
-
-void WIFI_get_connection( int socket_desc )
-{
-	struct sockaddr_in server;
-	server.sin_addr.s_addr = inet_addr("129.241.154.24");
-    // IP for PCen på skolen: 129.241.154.24
-	// IP address of google.com: 74.125.235.20 
-	server.sin_family = AF_INET;
-    server.sin_port = htons( 8888 );
-
-    if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        printf("connect error\n");
-    }
-     
-    printf("Connected\n");
-}
 
 
 int WIFI_send_data( int socket_desc )
@@ -73,16 +54,3 @@ int WIFI_send_data( int socket_desc )
     return 0;
 }
 
-
-int WIFI_receive_data( int socket_desc )
-{
-    char server_reply[2000];
-	if( recv(socket_desc, server_reply , 2000 , 0) < 0)
-    {
-        printf("recv failed\n");
-        return 1;
-    }
-    printf("Reply received\n");
-    //printf(server_reply);
-    return 0;
-}
