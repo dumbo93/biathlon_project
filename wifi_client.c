6dfa0b38a820253d7fa2ce95a383cd636de39329
dumbo93 <31391601+dumbo93@users.noreply.github.com>
2018-06-04 15:20:50 +0200

    Update wifi_client.c

--- wifi_client.c
+++ wifi_client.c
@@ -6,11 +6,11 @@
 #include "wifi_client.h"
 #include "central_state.h"
 
+int socket_desc;
 
-int WIFI_init( int socket_desc )
+int WIFI_init( void )
 {
 	printf("Hello from wifi_init\n");
-    	int sendt_data, received_data;
 	struct sockaddr_in server;
 	
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -25,7 +25,7 @@ int WIFI_init( int socket_desc )
 	server.sin_family = AF_INET;
     	server.sin_port = htons( 8888 );
 
-    	if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
+    	if (connect(&socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0)
    	{
         	printf("connect error\n");
 		return 0;
@@ -36,16 +36,16 @@ int WIFI_init( int socket_desc )
 }
 
 
-void WIFI_close_socket( int socket_desc )
+void WIFI_close_socket( void )
 {
-	close(socket_desc);
+	close(&socket_desc);
 }
 
 
 
-int WIFI_send_data( int socket_desc )
+int WIFI_send_data( void )
 {
-    if( send(socket_desc , &current_value_hex , strlen(&current_value_hex) , 0) < 0)
+    if( send(&socket_desc , &current_value_hex , strlen(&current_value_hex) , 0) < 0)
     {
         printf("Send failed\n");
         return 1;
